e8eaeec785a605c48d76a0b49c07d9a3070423e1
sueaoieong <41872158+sueaoieong@users.noreply.github.com>
2018-08-14 18:19:21 +0800

    Add files via upload

---
+++ people_show.py
@@ -0,0 +1,17 @@
+import sys
+from people_ui import Ui_Form
+from PyQt5.QtWidgets import QDialog,QApplication
+
+class people_show(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui=Ui_Form()
+        self.ui.setupUi(self)
+
+        self.show()
+
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    w=people_show()
+    sys.exit(app.exec_())
